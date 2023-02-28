ff970be9a0b967c1c75e3c5e10ae22cc64c188cf
Chel-Kakoito <114922636+kakoito-chel@users.noreply.github.com>
2023-02-28 16:09:08 +0300

    Update polakov.py

--- feb/28/polakov.py
+++ feb/28/polakov.py
@@ -16,3 +16,19 @@ for s in range(1,121):
         if f(s,'','',0,m)==1:
             print(s,m)
             break
+#4857
+def f(s,p,c,m):
+    if s <=54: return c % 2== m%2
+    if c==m: return 0
+
+    h = []
+    if p!='+1': h+=[f(s+1,'+1),c+1,m)]
+    if p!='+2': h += [f(s + 1, '+2', c + 1, m)]
+    if p!='*2': h += [f(s + 1, '*2', c + 1, m)]
+    return  any(h) if(c+1)%2==m%2 else all(h)
+
+for s in range(1,120):
+    for m in range(1,5):
+        if f(s,'',0,m)==1:
+            print(s,m)
+            break
